a93e2b41f460396581eb84d9672ecb0f0d760e66
rylee816 <65700028+rylee816@users.noreply.github.com>
2020-10-04 16:04:35 -0400

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((note, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((note, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            onDelete={deleteNote}
+            title={note.title}
+            content={note.content}
+          />
+        );
+      })}
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
